295a826ef278db98bb3054a0fd7d8fbb72efec01
pyphilia <pyphilia@gmail.com>
2022-02-18 10:31:34 +0100

    refactor: add file upload mutation, routines

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { default as configureQueryClient } from './queryClient';
 export * as Api from './api';
 export * as Hook from './hooks';
 export { API_ROUTES } from './api/routes';
+export * as ROUTINES from './routines';
