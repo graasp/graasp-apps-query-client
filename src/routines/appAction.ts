02985a1ee74b437bcdcff9dc999e89597438d616
spaenleh <basile.1358@gmail.com>
2022-06-27 09:37:35 +0200

    fix: create appAction routine argument

--- src/routines/appAction.ts
+++ src/routines/appAction.ts
@@ -1,4 +1,4 @@
 import { MUTATION_KEYS } from '../config/keys';
 import createRoutine from './utils';
 
-export const postAppActionRoutine = createRoutine(MUTATION_KEYS.POST_APP_ACTION);
+export const postAppActionRoutine = createRoutine(MUTATION_KEYS.POST_APP_ACTION[0]);
