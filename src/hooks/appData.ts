a56bf9cbac1bcabff7e4ccaae9db42c25fd6e79f
spaenleh <basile.1358@gmail.com>
2022-07-08 11:15:53 +0200

    fix: add return type of useAppContext

--- src/hooks/appData.ts
+++ src/hooks/appData.ts
@@ -1,10 +1,10 @@
-import { List } from 'immutable';
+import { List, RecordOf } from 'immutable';
 import { QueryClient, useQuery } from 'react-query';
 import * as Api from '../api';
 import { MissingFileIdError } from '../config/errors';
 import { buildAppContextKey, buildAppDataKey, buildFileContentKey } from '../config/keys';
 import { getApiHost, getDataOrThrow } from '../config/utils';
-import { AppContextRecord, QueryClientConfig } from '../types';
+import { AppContext, AppContextRecord, QueryClientConfig } from '../types';
 
 export default (queryClient: QueryClient, queryConfig: QueryClientConfig) => {
   const { retry, cacheTime, staleTime } = queryConfig;
@@ -33,8 +33,12 @@ export default (queryClient: QueryClient, queryConfig: QueryClientConfig) => {
       const { token, itemId } = getDataOrThrow(queryClient);
       return useQuery({
         queryKey: buildAppContextKey(itemId),
-        queryFn: () => {
-          return Api.getContext({ itemId, token, apiHost }).then((data) => AppContextRecord(data));
+        queryFn: (): Promise<RecordOf<AppContext>> => {
+          return Api.getContext({
+            itemId,
+            token,
+            apiHost,
+          }).then((data) => AppContextRecord(data));
         },
         ...defaultOptions,
         enabled: Boolean(itemId) && Boolean(token),
