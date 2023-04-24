337310522d963b3e0be815f4a3d926e1f84a3e4d
spaenleh <spaenleh@gmail.com>
2023-04-24 16:16:05 +0200

    fix: update packages

--- jest.config.ts
+++ jest.config.ts
@@ -74,11 +74,11 @@ export default {
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'node'],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  moduleNameMapper: {
-    '^react$': '<rootDir>/example/node_modules/react',
-    '^react-dom$': '<rootDir>/example/node_modules/react-dom',
-    '^react-dom/(.*)': '<rootDir>/example/node_modules/react-dom/$1',
-  },
+  // moduleNameMapper: {
+  //   '^react$': '<rootDir>/example/node_modules/react',
+  //   '^react-dom$': '<rootDir>/example/node_modules/react-dom',
+  //   '^react-dom/(.*)': '<rootDir>/example/node_modules/react-dom/$1',
+  // },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'apps-query-client',
+      formats: ['cjs', 'es'],
       fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {
