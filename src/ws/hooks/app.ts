03daa7510072830fa80b0fe5eca247b19b7f7931
Jérémy La Scala <jeremy.lascala@epfl.ch>
2024-02-15 15:27:29 +0100

    refactor(ws): move hook outside of config func
    move the useAppDataUpdates outside of the
    configureWsAppDataHooks func
    reduces the number of levels of nested
    functions

--- src/ws/hooks/app.ts
+++ src/ws/hooks/app.ts
@@ -19,71 +19,73 @@ import {
 } from '../types';
 import { Channel, WebsocketClient } from '../ws-client';
 
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export const configureWsAppDataHooks = (websocketClient?: WebsocketClient) => ({
-  /**
-   * React hook to subscribe to the updates of the app data for
-   * the given item ID.
-   * @param itemId The ID of the item of which to observe updates
-   */
-  useAppDataUpdates: (itemId?: UUID | null) => {
-    const queryClient = useQueryClient();
-    useEffect(() => {
-      if (!websocketClient) {
-        // do nothing
-        console.warn('No websocket client was provided.');
-        return () => undefined;
-      }
-      if (!itemId) {
-        // do nothing
-        return () => undefined;
-      }
+/**
+ * React hook to subscribe to the updates of the app data for
+ * the given item ID.
+ * @param itemId The ID of the item of which to observe updates
+ */
+const useAppDataUpdates = (itemId?: UUID | null, websocketClient?: WebsocketClient) => {
+  const queryClient = useQueryClient();
+  useEffect(() => {
+    if (!websocketClient) {
+      // do nothing
+      console.warn('No websocket client was provided.');
+      return () => undefined;
+    }
+    if (!itemId) {
+      // do nothing
+      return () => undefined;
+    }
 
-      const channel: Channel = { name: itemId, topic: APP_DATA_TOPIC };
-      const appDataKey = appDataKeys.single(itemId);
+    const channel: Channel = { name: itemId, topic: APP_DATA_TOPIC };
+    const appDataKey = appDataKeys.single(itemId);
 
-      const handler = (event: AppDataEvent): void => {
-        if (event.kind === AppEventKinds.AppData) {
-          const appDataList = queryClient.getQueryData<AppData[]>(appDataKey);
-          const newAppData: AppData = event.appData;
-          switch (event.op) {
-            case AppOperations.POST: {
-              if (!appDataList?.some(({ id }) => id === newAppData.id))
-                queryClient.setQueryData(
-                  appDataKey,
-                  appDataList ? [...(appDataList ?? []), newAppData] : [newAppData],
-                );
-              break;
-            }
-            case AppOperations.PATCH: {
-              if (appDataList) {
-                const newAppDataList = appDataList.filter((a) => a.id !== newAppData.id);
-                newAppDataList.push(newAppData);
-                queryClient.setQueryData(appDataKey, newAppDataList);
-              }
-              break;
-            }
-            case AppOperations.DELETE: {
+    const handler = (event: AppDataEvent): void => {
+      if (event.kind === AppEventKinds.AppData) {
+        const appDataList = queryClient.getQueryData<AppData[]>(appDataKey);
+        const newAppData: AppData = event.appData;
+        switch (event.op) {
+          case AppOperations.POST: {
+            if (!appDataList?.some(({ id }) => id === newAppData.id))
               queryClient.setQueryData(
                 appDataKey,
-                appDataList?.filter(({ id }) => id !== newAppData.id),
+                appDataList ? [...(appDataList ?? []), newAppData] : [newAppData],
               );
-              break;
-            }
-            default:
-              console.warn('unhandled event for useAppDataUpdates');
-              break;
+            break;
           }
+          case AppOperations.PATCH: {
+            if (appDataList) {
+              const newAppDataList = appDataList.filter((a) => a.id !== newAppData.id);
+              newAppDataList.push(newAppData);
+              queryClient.setQueryData(appDataKey, newAppDataList);
+            }
+            break;
+          }
+          case AppOperations.DELETE: {
+            queryClient.setQueryData(
+              appDataKey,
+              appDataList?.filter(({ id }) => id !== newAppData.id),
+            );
+            break;
+          }
+          default:
+            console.warn('unhandled event for useAppDataUpdates');
+            break;
         }
-      };
-      websocketClient.subscribe(channel, handler);
+      }
+    };
+    websocketClient.subscribe(channel, handler);
 
-      return () => {
-        websocketClient.unsubscribe(channel, handler);
-      };
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [itemId]);
-  },
+    return () => {
+      websocketClient.unsubscribe(channel, handler);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [itemId]);
+};
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+export const configureWsAppDataHooks = (websocketClient?: WebsocketClient) => ({
+  useAppDataUpdates: (itemId?: UUID | null) => useAppDataUpdates(itemId, websocketClient),
 });
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
